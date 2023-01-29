90badef0b137fc1a0c3d8d2340562667a5763b92
Isaac Lichtenstein <islichtenstein@gmail.com>
2023-01-29 17:53:31 +0100

    fixing the coachs response

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,8 +6,8 @@ class QuestionsController < ApplicationController
     @question = params[:question] # the question they asked
 
     if @question == "I am going to work"
-      @answer = `Great!`
-    elsif @question.end_with?(`?`)
+      @answer = "Great!"
+    elsif @question.end_with?("?")
       @answer = "Silly question, get dressed and go to work!"
     else
       @answer = "I don't care, get dressed and go to work!"
