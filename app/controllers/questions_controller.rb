e04e5ed4c731bcc5fb1c0760316c61d2056b6ba2
Isaac Lichtenstein <islichtenstein@gmail.com>
2023-01-29 17:59:59 +0100

    making the coach task a bit neater

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,11 +3,12 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @question = params[:question] # the question they asked
-
-    if @question == "I am going to work"
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
       @answer = "Great!"
-    elsif @question.end_with?("?")
+    elsif @question.ends_with?("?")
       @answer = "Silly question, get dressed and go to work!"
     else
       @answer = "I don't care, get dressed and go to work!"
